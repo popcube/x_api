f712f658566ff595d57f9c3ba76200d3d7e6ef6d
popcube <40394657+popcube@users.noreply.github.com>
2022-12-07 18:41:45 +0900

    get_data.js created

---
+++ src/get_data.js
@@ -0,0 +1,28 @@
+//Import package
+import { Client, auth } from "twitter-api-sdk";
+
+// Initialize auth client first
+// const authClient = new auth.OAuth2User({
+//     client_id: process.env.CLIENT_ID,
+//     client_secret: process.env.CLIENT_SECRET,
+//     callback: "YOUR-CALLBACK",
+//     scopes: ["tweet.read", "users.read", "offline.access"],
+// });
+
+// Pass auth credentials to the library client 
+const client = new Client(process.env.BEARER_TOKEN);
+const params = {
+    "user.fields": "public_metrics,verified",
+}
+
+
+const main = async () => {
+    try {
+        const userObj = await client.users.findUserByUsername("pj_sekai", params);
+        console.log(userObj);
+    } catch (error) {
+        console.log("tweets error", error);
+    }
+}
+
+export default main;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
 //Import package
-import { Client, auth } from "twitter-api-sdk";
+// import { Client, auth } from "twitter-api-sdk";
+import Get_data from "./get_data.js"
 
-// Initialize auth client first
-// const authClient = new auth.OAuth2User({
-//     client_id: process.env.CLIENT_ID,
-//     client_secret: process.env.CLIENT_SECRET,
-//     callback: "YOUR-CALLBACK",
-//     scopes: ["tweet.read", "users.read", "offline.access"],
-// });
-
-// Pass auth credentials to the library client 
-const client = new Client(process.env.BEARER_TOKEN);
-const params = {
-    "user.fields": "public_metrics,verified",
-}
-
-
-async function main() {
-    try {
-        const userObj = await client.users.findUserByUsername("pj_sekai", params);
-        console.log(userObj);
-    } catch (error) {
-        console.log("tweets error", error);
-    }
-}
-
-main()
+Get_data();
