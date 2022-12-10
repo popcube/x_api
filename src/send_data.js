515dd20a02f14ec26f79405657c4bb422d2a4aac
popcube <40394657+popcube@users.noreply.github.com>
2022-12-10 14:39:26 +0900

    debug

--- src/send_data.js
+++ src/send_data.js
@@ -12,7 +12,7 @@ const documentClient = new AWS.DynamoDB({
 const params = {
     Statements: [
         {
-            Statement: 'SELECT * FROM "twt_api_pjsekai" WHERE fetch_time = "2022-12-07T17:52:00"'
+            Statement: 'SELECT * FROM "twt_api_pjsekai" WHERE "fetch_time" = "2022-12-07T17:52:00"'
         }
     ]
 }
