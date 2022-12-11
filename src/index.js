619c89948aaa4b360bc050377e594a061e636edb
popcube <40394657+popcube@users.noreply.github.com>
2022-12-11 17:38:54 +0900

    debug

--- src/index.js
+++ src/index.js
@@ -4,18 +4,23 @@ import get_data from "./get_data.js"
 import send_data from "./send_data.js"
 
 const makeDynObj = (twtObj) => {
-    const fetchTime = new Date().toISOString().slice(0, -2);
 
-    return {
-        TableName: "twt_api_pjsekai",
-        Item: {
-            fetch_time: { S: fetchTime }
-        }
-    };
+    return new Promise((resolve) => {
+        const fetchTime = new Date().toISOString().slice(0, -2);
+        resolve(
+            {
+                TableName: "twt_api_pjsekai",
+                Item: {
+                    fetch_time: { S: fetchTime }
+                }
+            }
+        )
+    });
 }
 
 
 
 
-// const twtObj = get_data();
-send_data();
+const twtObj = get_data();
+const dynObj = await makeDynObj(twtObj)
+send_data(dynObj);
