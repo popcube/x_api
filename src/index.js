54bc7217808835c1a1f371d80abe2168d1656de9
hidenori-nishimura <hidenori_nishimura@mail.toyota.co.jp>
2022-12-12 20:11:19 +0900

    debug

--- src/index.js
+++ src/index.js
@@ -5,35 +5,44 @@ import send_data from "./send_data.js"
 
 const makeDynObj = (twtObj) => {
 
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         const fetchTime = new Date().toISOString().slice(0, -2);
         const twtData = twtObj.data;
         // console.log(JSON.stringify(twtObj, null, 2));
         const twtDataPM = twtData.public_metrics;
-        resolve(
+        if (twtData.verified) {
+            reject("this twitter account is not verified");
+        } else {
+            resolve(
             {
                 TableName: "twt_api_pjsekai",
                 Item: {
                     fetch_time: { S: fetchTime },
-                    data: {
-                        M: {
-                            username: { S: twtData.username },
-                            id: { S: twtData.id },
-                            name: { S: twtData.name },
-                            // public_metrics: {
-                            //     M: {
-                            //         followers_count: { N: twtDataPM.followers_count },
-                            //         following_count: { N: twtDataPM.following_count },
-                            //         listed_count: { N: twtDataPM.listed_count },
-                            //         tweet_count: { N: twtDataPM.tweet_count }
-                            //     }
-                            // },
-                            verified: { BOOL: twtData.verified }
-                        }
-                    }
+                    followers_count: { N: twtDataPM.followers_count.toString() },
+                    following_count: { N: twtDataPM.following_count.toString() },
+                    listed_count: { N: twtDataPM.listed_count.toString() },
+                    tweet_count: { N: twtDataPM.tweet_count.toString() }
+                    },
+                    // data: {
+                    //     M: {
+                    //         username: { S: twtData.username },
+                    //         id: { S: twtData.id },
+                    //         name: { S: twtData.name },
+                    //         public_metrics: {
+                    //             M: {
+                    //                 followers_count: { N: twtDataPM.followers_count.toString() },
+                    //                 following_count: { N: twtDataPM.following_count.toString() },
+                    //                 listed_count: { N: twtDataPM.listed_count.toString() },
+                    //                 tweet_count: { N: twtDataPM.tweet_count.toString() }
+                    //             }
+                    //         },
+                    //         verified: { BOOL: twtData.verified }
+                    //     }
+                    // }
                 }
             }
-        )
+        );
+        }
     })
 };
 
