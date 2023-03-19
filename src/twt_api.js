6229776891d5d8fad239453378c1603238d159b8
popcube <40394657+popcube@users.noreply.github.com>
2023-03-19 11:47:26 +0900

    debug

--- src/twt_api.js
+++ src/twt_api.js
@@ -32,8 +32,9 @@ export const getFollowers = async (userName) => {
 
 export const getTweets = async (id) => {
     try {
+        delete paramsTweets["pagination_token"];
         var tweetObj = await client.tweets.usersIdTweets(id, paramsTweets);
-        console.log(tweetObj);
+        // console.log(tweetObj);
         const resArr = tweetObj.data;
         console.log("Tweets data received at " + new Date().toISOString().slice(0, -2));
         // console.log("meta: " + JSON.stringify(tweetObj.meta, null, 2))
