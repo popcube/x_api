219552e38ded20bcd2b97a234ea412b10de60bdb
popcube <40394657+popcube@users.noreply.github.com>
2023-03-19 11:58:09 +0900

    debug

--- src/index_2.js
+++ src/index_2.js
@@ -6,52 +6,58 @@ import fs from "fs"
 
 const scanParam = { TableName: "twt_main_flwers" };
 const userNames = ["pj_sekai", "bang_dream_gbp", "genshin_7"];
-// const userNames = ["bang_dream_gbp", "genshin_7"];
 
-for (const userName of userNames) {
-    const flwObj = await getFollowers(userName);
-    /* DEPRECATED to be deleted*/
-    // const dynObj = await makeFollowersDynObj(flwObj)
-    // sendDyn(dynObj);
-    /* DEPRECATED to be deleted*/
+async function main() {
+    // FOR SKIPPING LOOP
+    userNames.splice(0);
 
-    console.log("User id: " + flwObj.data.id);
-    const twtArr = await getTweets(flwObj.data.id);
-    // console.log(typeof (twtArr))
-    console.log(JSON.stringify(twtArr.slice(-1, twtArr.length), null, 2));
-    console.log("Tweet count size: " + twtArr.length);
+    for (const userName of userNames) {
+        const flwObj = await getFollowers(userName);
+        /* DEPRECATED to be deleted*/
+        // const dynObj = await makeFollowersDynObj(flwObj)
+        // sendDyn(dynObj);
+        /* DEPRECATED to be deleted*/
 
-    for (const twt of twtArr.slice(-2, twtArr.length)) {
-        console.log(`${twt.created_at} https://twitter.com/${userName}/status/${twt.id}`)
-    };
+        console.log("User id: " + flwObj.data.id);
+        const twtArr = await getTweets(flwObj.data.id);
+        // console.log(typeof (twtArr))
+        console.log(JSON.stringify(twtArr.slice(-1, twtArr.length), null, 2));
+        console.log("Tweet count size: " + twtArr.length);
 
-    const twtArrRef = twtArr.filter(el => "referenced_tweets" in el);
-    console.log("Referenced tweet count size: " + twtArrRef.length);
-    const twtArrRefMulti = twtArrRef.filter(el => el.referenced_tweets.length > 1);
-    console.log("Multi referenced tweet count size: " + twtArrRefMulti.length);
+        for (const twt of twtArr.slice(-2, twtArr.length)) {
+            console.log(`${twt.created_at} https://twitter.com/${userName}/status/${twt.id}`)
+        };
 
-    for (const twt of twtArrRef.slice(-2, twtArrRef.length)) {
-        console.log(`${twt.created_at} https://twitter.com/${userName}/status/${twt.id}`)
-        console.log(twt.referenced_tweets)
-    };
+        const twtArrRef = twtArr.filter(el => "referenced_tweets" in el);
+        console.log("Referenced tweet count size: " + twtArrRef.length);
+        const twtArrRefMulti = twtArrRef.filter(el => el.referenced_tweets.length > 1);
+        console.log("Multi referenced tweet count size: " + twtArrRefMulti.length);
 
-    const twtCsv = twtArr.reduce((prev, curr) => {
-        const referenced = "referenced_tweets" in curr ? curr.referenced_tweets[0].type.slice(0, 3).toUpperCase() : "ORG"
-        prev += `${curr.created_at.slice(0, -2)},${referenced},https://twitter.com/${userName}/status/${curr.id}\n`;
-        return prev;
-    }, 'time,referenced,url\n');
-    fs.writeFileSync("./twtResults.csv", twtCsv);
+        for (const twt of twtArrRef.slice(-2, twtArrRef.length)) {
+            console.log(`${twt.created_at} https://twitter.com/${userName}/status/${twt.id}`)
+            console.log(twt.referenced_tweets)
+        };
 
-    // DO NOT DELETE BELOW
+        const twtCsv = twtArr.reduce((prev, curr) => {
+            const referenced = "referenced_tweets" in curr ? curr.referenced_tweets[0].type.slice(0, 3).toUpperCase() : "ORG"
+            prev += `${curr.created_at.slice(0, -2)},${referenced},https://twitter.com/${userName}/status/${curr.id}\n`;
+            return prev;
+        }, 'time,referenced,url\n');
+        fs.writeFileSync(`./twtResults_${userName}.csv`, twtCsv);
 
-    if (false) {
-        const dynScan = await scanDyn(scanParam);
+        // DO NOT DELETE BELOW
+    }
 
+    const dynScan = await scanDyn(scanParam);
+    console.log(dynScan);
+
+    for (const userName of userNames) {
         const outputCsv = dynScan.reduce((prev, curr) => {
             prev += `${curr["fetch_time"]["S"]},${curr["followers_count"]["N"]}\n`;
             return prev;
         }, 'fetch_time,followers_count\n');
         fs.writeFileSync(`./results_${userName}.csv`, outputCsv);
     }
-
 }
+
+main()
