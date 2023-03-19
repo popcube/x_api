5b96a6122e396704392907c49e7612ea188e3888
popcube <40394657+popcube@users.noreply.github.com>
2023-03-19 12:43:50 +0900

    debug

--- src/index.js
+++ src/index.js
@@ -4,70 +4,62 @@ import { getFollowers, getTweets } from "./twt_api.js"
 import { sendDyn, scanDyn } from "./dyn_api.js"
 import fs from "fs"
 
-const makeFollowersDynObj = (twtObj) => {
+const scanParam = { TableName: "twt_main_flwers" };
+const userNames = ["pj_sekai", "bang_dream_gbp", "Genshin_7"];
 
-    return new Promise((resolve, reject) => {
-        const fetchTime = new Date().toISOString().slice(0, -2);
-        const twtData = twtObj.data;
-        console.log(JSON.stringify(twtObj, null, 2));
-        const twtDataPM = twtData.public_metrics;
-        if (!twtData.verified) {
-            reject("this twitter account is not verified");
-        } else {
-            resolve(
-                {
-                    TableName: "twt_api_pjsekai",
-                    Item: {
-                        fetch_time: { S: fetchTime },
-                        followers_count: { N: twtDataPM.followers_count.toString() }
-                    }
-                }
-            );
-        }
-    })
-};
+async function main() {
+    // FOR SKIPPING LOOP
+    // userNames.splice(0);
 
-const scanParam = { TableName: "twt_api_1min" };
-const userName = "pj_sekai"
+    for (const userName of userNames) {
+        const flwObj = await getFollowers(userName);
+        /* DEPRECATED to be deleted*/
+        // const dynObj = await makeFollowersDynObj(flwObj)
+        // sendDyn(dynObj);
+        /* DEPRECATED to be deleted*/
 
-const flwObj = await getFollowers(userName);
-/* DEPRECATED to be deleted*/
-// const dynObj = await makeFollowersDynObj(flwObj)
-// sendDyn(dynObj);
-/* DEPRECATED to be deleted*/
+        console.log("User id: " + flwObj.data.id);
+        const twtArr = await getTweets(flwObj.data.id);
+        // console.log(typeof (twtArr))
+        console.log(JSON.stringify(twtArr.slice(-1, twtArr.length), null, 2));
+        console.log("Tweet count size: " + twtArr.length);
 
-console.log("User id: " + flwObj.data.id);
-const twtArr = await getTweets(flwObj.data.id);
-console.log(JSON.stringify(twtArr.slice(-1, twtArr.length), null, 2));
-console.log("Tweet count size: " + twtArr.length);
+        for (const twt of twtArr.slice(-2, twtArr.length)) {
+            console.log(`${twt.created_at} https://twitter.com/${userName}/status/${twt.id}`)
+        };
 
-for (const twt of twtArr.slice(-2, twtArr.length)) {
-    console.log(`${twt.created_at} https://twitter.com/${userName}/status/${twt.id}`)
-};
+        const twtArrRef = twtArr.filter(el => "referenced_tweets" in el);
+        console.log("Referenced tweet count size: " + twtArrRef.length);
+        const twtArrRefMulti = twtArrRef.filter(el => el.referenced_tweets.length > 1);
+        console.log("Multi referenced tweet count size: " + twtArrRefMulti.length);
 
-const twtArrRef = twtArr.filter(el => "referenced_tweets" in el);
-console.log("Referenced tweet count size: " + twtArrRef.length);
-const twtArrRefMulti = twtArrRef.filter(el => el.referenced_tweets.length > 1);
-console.log("Multi referenced tweet count size: " + twtArrRefMulti.length);
+        for (const twt of twtArrRef.slice(-2, twtArrRef.length)) {
+            console.log(`${twt.created_at} https://twitter.com/${userName}/status/${twt.id}`)
+            console.log(twt.referenced_tweets)
+        };
 
-for (const twt of twtArrRef.slice(-2, twtArrRef.length)) {
-    console.log(`${twt.created_at} https://twitter.com/${userName}/status/${twt.id}`)
-    console.log(twt.referenced_tweets)
-};
+        const twtCsv = twtArr.reduce((prev, curr) => {
+            const referenced = "referenced_tweets" in curr ? curr.referenced_tweets[0].type.slice(0, 3).toUpperCase() : "ORG"
+            prev += `${curr.created_at.slice(0, -2)},${referenced},https://twitter.com/${userName}/status/${curr.id}\n`;
+            return prev;
+        }, 'time,referenced,url\n');
+        fs.writeFileSync(`./twtResults_${userName}.csv`, twtCsv);
 
-const twtCsv = twtArr.reduce((prev, curr) => {
-    const referenced = "referenced_tweets" in curr ? curr.referenced_tweets[0].type.slice(0, 3).toUpperCase() : "ORG"
-    prev += `${curr.created_at.slice(0, -2)},${referenced},https://twitter.com/${userName}/status/${curr.id}\n`;
-    return prev;
-}, 'time,referenced,url\n');
-fs.writeFileSync("./twtResults.csv", twtCsv);
+        // DO NOT DELETE BELOW
+    }
 
-// DO NOT DELETE BELOW
+    const dynScan = await scanDyn(scanParam);
+    // console.log(dynScan);
 
-const dynScan = await scanDyn(scanParam);
+    for (const userName of userNames) {
+        const outputCsv = dynScan.reduce((prev, curr) => {
+            if (curr["account"]["S"] == userName) {
+                prev += `${curr["fetch_time"]["S"]},${curr["followers_count"]["N"]}\n`;
+            }
+            return prev;
+        }, 'fetch_time,followers_count\n');
+        fs.writeFileSync(`./results_${userName}.csv`, outputCsv);
+    }
+}
 
-const outputCsv = dynScan.reduce((prev, curr) => {
-    prev += `${curr["fetch_time"]["S"]},${curr["followers_count"]["N"]}\n`;
-    return prev;
-}, 'fetch_time,followers_count\n');
-fs.writeFileSync("./results.csv", outputCsv);
+main()
---
+++ src/index_old.js
@@ -0,0 +1,73 @@
+//Import package
+// import { Client, auth } from "twitter-api-sdk";
+import { getFollowers, getTweets } from "./twt_api.js"
+import { sendDyn, scanDyn } from "./dyn_api.js"
+import fs from "fs"
+
+const makeFollowersDynObj = (twtObj) => {
+
+    return new Promise((resolve, reject) => {
+        const fetchTime = new Date().toISOString().slice(0, -2);
+        const twtData = twtObj.data;
+        console.log(JSON.stringify(twtObj, null, 2));
+        const twtDataPM = twtData.public_metrics;
+        if (!twtData.verified) {
+            reject("this twitter account is not verified");
+        } else {
+            resolve(
+                {
+                    TableName: "twt_api_pjsekai",
+                    Item: {
+                        fetch_time: { S: fetchTime },
+                        followers_count: { N: twtDataPM.followers_count.toString() }
+                    }
+                }
+            );
+        }
+    })
+};
+
+const scanParam = { TableName: "twt_api_1min" };
+const userName = "pj_sekai"
+
+const flwObj = await getFollowers(userName);
+/* DEPRECATED to be deleted*/
+// const dynObj = await makeFollowersDynObj(flwObj)
+// sendDyn(dynObj);
+/* DEPRECATED to be deleted*/
+
+console.log("User id: " + flwObj.data.id);
+const twtArr = await getTweets(flwObj.data.id);
+console.log(JSON.stringify(twtArr.slice(-1, twtArr.length), null, 2));
+console.log("Tweet count size: " + twtArr.length);
+
+for (const twt of twtArr.slice(-2, twtArr.length)) {
+    console.log(`${twt.created_at} https://twitter.com/${userName}/status/${twt.id}`)
+};
+
+const twtArrRef = twtArr.filter(el => "referenced_tweets" in el);
+console.log("Referenced tweet count size: " + twtArrRef.length);
+const twtArrRefMulti = twtArrRef.filter(el => el.referenced_tweets.length > 1);
+console.log("Multi referenced tweet count size: " + twtArrRefMulti.length);
+
+for (const twt of twtArrRef.slice(-2, twtArrRef.length)) {
+    console.log(`${twt.created_at} https://twitter.com/${userName}/status/${twt.id}`)
+    console.log(twt.referenced_tweets)
+};
+
+const twtCsv = twtArr.reduce((prev, curr) => {
+    const referenced = "referenced_tweets" in curr ? curr.referenced_tweets[0].type.slice(0, 3).toUpperCase() : "ORG"
+    prev += `${curr.created_at.slice(0, -2)},${referenced},https://twitter.com/${userName}/status/${curr.id}\n`;
+    return prev;
+}, 'time,referenced,url\n');
+fs.writeFileSync("./twtResults.csv", twtCsv);
+
+// DO NOT DELETE BELOW
+
+const dynScan = await scanDyn(scanParam);
+
+const outputCsv = dynScan.reduce((prev, curr) => {
+    prev += `${curr["fetch_time"]["S"]},${curr["followers_count"]["N"]}\n`;
+    return prev;
+}, 'fetch_time,followers_count\n');
+fs.writeFileSync("./results.csv", outputCsv);
