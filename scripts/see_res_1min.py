c2017f0b88bff6a925065fa7f5e4acc1f821d788
popcube <40394657+popcube@users.noreply.github.com>
2023-09-02 14:09:05 +0900

    debug

--- scripts/see_res_1min.py
+++ scripts/see_res_1min.py
@@ -339,12 +339,14 @@ while ni < len(nan_idxs) - 1:
 
 df = pd.DataFrame([x_dif, y_cut_dif]).T
 df.columns = ["time", "y_cut_diff"]
+print(df["time"].dtype)
+print(dt["time"])
 
 df.drop(nan_idxs, inplace=True)
 
 # this date format is needed to preserve the format
 # df.to_csv("./result_cut_dif.csv", index=False,
 #           date_format="%Y-%m-%d %H:%M:%S.%f")
-df["time"].dt.strftime("%Y-%m-%d %H:%M:%S.%f")
+# df["time"].dt.strftime("%Y-%m-%d %H:%M:%S.%f")
 df.to_csv("./result_cut_dif.csv", index=False)
 print("result_cut_dif.csv is saved!")
